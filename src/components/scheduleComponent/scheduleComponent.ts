ab2fe0361dbc9fa9aaa955349a756b1359642b7b
Fort163 <Front-91@mail.ru>
2025-10-18 11:46:27 +0400

    create ScheduleComponent

---
+++ src/components/scheduleComponent/scheduleComponent.ts
@@ -0,0 +1,56 @@
+import Component from "vue-class-component";
+import {Prop} from "vue-property-decorator";
+import {RestrictionFactory} from "@/store/restriction/RestrictionFactory";
+import {CheckComponent} from "@/store/component";
+import InputSchedule from "@/components/inputSchedule/InputSchedule.vue";
+import Button from "@/components/button/Button.vue";
+import {Schedule} from "@/models/company-service";
+
+@Component({
+    components: {
+        InputSchedule,
+        Button
+    }
+})
+export default class ScheduleComponent extends CheckComponent {
+
+
+    @Prop() private value : Array<Schedule> | undefined;
+
+    @Prop() private needRepeat : boolean | undefined;
+
+    private keyNumber : number = 0;
+
+    private currentValue : Array<Schedule> | undefined;
+
+    private restriction : RestrictionFactory = new RestrictionFactory();
+
+    created(){
+        if(this.value) {
+            this.currentValue = this.value
+        }
+    }
+
+    public scheduleCheck(){
+        super.check();
+    }
+
+    public repeat(index : number){
+        if(this.currentValue) {
+            const fromItem = this.currentValue[index];
+            this.currentValue?.forEach((item, i) => {
+                if (index < i && this.currentValue){
+                    this.currentValue[i].clockFrom = fromItem.clockFrom;
+                    this.currentValue[i].clockTo = fromItem.clockTo;
+                }
+            })
+        }
+        this.keyNumber++
+    }
+
+    getValue(): any {
+        return this.currentValue
+    }
+
+
+}
